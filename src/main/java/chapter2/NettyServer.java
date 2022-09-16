6976a48ee2b37b18f8b3ce7aaea001fb9cc52d9b
dulu <810672731@qq.com>
2022-09-16 22:59:47 +0800

    Initial commit 第2章，客户端和服务端的启动

---
+++ src/main/java/chapter2/NettyServer.java
@@ -0,0 +1,35 @@
+package chapter2;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+
+public class NettyServer {
+    public static void main(String[] args) {
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+        // boss 负责创建连接
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        // worker 负责读取数据和业务逻辑处理
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        serverBootstrap.group(boss, worker)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) {
+                        ch.pipeline().addLast(new StringDecoder());
+                        ch.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
+                            @Override
+                            protected void channelRead0(ChannelHandlerContext channelHandlerContext, String msg) throws Exception {
+                                System.out.println(msg);
+                            }
+                        });
+                    }
+                }).bind(8000);
+    }
+}
