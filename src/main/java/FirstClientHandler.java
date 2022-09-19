464a30840253f781d0f25ce593713c6d86cfd0fe
dulu01 <dulu01@meicai.cn>
2022-09-19 22:00:06 +0800

    6.1 客户端发送数据到服务端 6.2 服务端返回数据到客户端

--- src/main/java/FirstClientHandler.java
+++ src/main/java/FirstClientHandler.java
@@ -2,31 +2,36 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
-import java.nio.charset.StandardCharsets;
+import java.nio.charset.Charset;
 import java.util.Date;
 
 public class FirstClientHandler extends ChannelInboundHandlerAdapter {
-    /**
-     * 此方法会在客户端连接建立成功之后被调用
-     *
-     * @param ctx
-     */
     @Override
-    public void channelInactive(ChannelHandlerContext ctx) {
-        System.out.println(new Date() + ":客户端写出数据");
-        // 获取客户端的数据
+    public void channelActive(ChannelHandlerContext ctx) {
+        System.out.println(new Date() + ": 客户端写出数据");
+
+        // 1.获取数据
         ByteBuf buffer = getByteBuf(ctx);
-        // 向服务端写数据
+
+        // 2.写数据
         ctx.channel().writeAndFlush(buffer);
     }
 
     private ByteBuf getByteBuf(ChannelHandlerContext ctx) {
+        byte[] bytes = "你好，闪电侠!".getBytes(Charset.forName("utf-8"));
+
         ByteBuf buffer = ctx.alloc().buffer();
-        // 获取二进制抽象对象
-        // 准备数据，指定字符串的字符集为utf-8
-        byte[] bytes = "你好,闪电侠!".getBytes(StandardCharsets.UTF_8);
-        // 填充数据到  ByteBuf
+
         buffer.writeBytes(bytes);
+
         return buffer;
+    }
+
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf byteBuf = (ByteBuf) msg;
+
+        System.out.println(new Date() + ": 客户端读到数据 -> " + byteBuf.toString(Charset.forName("utf-8")));
     }
 }
--- src/main/java/NettyClient.java
+++ src/main/java/NettyClient.java
@@ -1,7 +1,8 @@
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
 import java.util.concurrent.TimeUnit;
@@ -15,19 +16,23 @@ public class NettyClient {
         NioEventLoopGroup workerGroup = new NioEventLoopGroup();
         bootstrap.group(workerGroup)
                 .channel(NioSocketChannel.class)
-                .handler(new ChannelInitializer<Channel>() {
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .option(ChannelOption.SO_KEEPALIVE, true)
+                .option(ChannelOption.TCP_NODELAY, true)
+                .handler(new ChannelInitializer<SocketChannel>() {
                     @Override
-                    protected void initChannel(Channel ch) {
+                    public void initChannel(SocketChannel ch) {
                         ch.pipeline().addLast(new FirstClientHandler());
                     }
                 });
-        connect(bootstrap, "meituan.com", 80, MAX_RETRY);
+
+        connect(bootstrap, "127.0.0.1", 8080, MAX_RETRY);
     }
 
     private static void connect(Bootstrap bootstrap, String host, final int port, final int retry) {
         bootstrap.connect(host, port).addListener(future -> {
             if (future.isSuccess()) {
-                System.out.println("连接成功");
+                System.out.println("连接成功" + host + ":" + port);
             } else if (retry == 0) {
                 System.out.println("重连次数用完，放弃连接");
             } else {
